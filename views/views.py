677897b4dfc8ecbaf4d98e2676bb5e1b5a9a7a90
Matthew Blau <matthew@saphira.mrb1105.gmail.com.beta.tailscale.net>
2022-07-31 18:25:57 -0400

    fixed bug where deletion was not working

--- views/views.py
+++ views/views.py
@@ -112,7 +112,7 @@ async def update_or_delete_transaction(request):
                                                    amount=float(data['newamount']), note=data['newname'], date_of_transactions=date,
                                                    user_id=session_user['sub'], old_category_id=transaction_id, categories=data['category'], submit_time=data['submitTime'])
             elif 'btnDeleteTransaction' in data:
-                transaction_id = await transaction.get_transaction_id(session_user['sub'], data['oldamount'], data['oldname'], datetime(old_date), data['oldcategory'], data['submitTime'])
+                transaction_id = await transaction.get_transaction_id(user_id=session_user['sub'], recipient=data['old_recipient'], amount=data['oldamount'], note=data['oldname'], date=old_date, category=data['oldcategory'], submit_time=data['submitTime'])
                 await transaction.delete_transaction(
                     session_user['sub'], transaction_id)
                 await user.update_balance(session_user['sub'], await transaction.sum_of_transactions(session_user['sub']))
