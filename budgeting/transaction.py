ab8d4fd7d6a56fb93cceccd12211d202b1715380
Matthew Blau <matthew@saphira.mrb1105.gmail.com.beta.tailscale.net>
2022-07-31 15:03:34 -0400

    explictly called params

--- budgeting/transaction.py
+++ budgeting/transaction.py
@@ -41,8 +41,6 @@ class Transactions():
 
     async def get_transaction_id(self, user_id, amount: float, note: str, date: datetime, category: Categories, submit_time: datetime):
         async with Session() as session:
-            print(type(date))
-            print(type(Transaction.date))
             transaction_id = await session.execute(select(Transaction.id).where(Transaction.user_id == str(user_id)).where(Transaction.amount == float(amount)).where(Transaction.date == date).where(Transaction.categories == category).where(Transaction.note == str(note)).where(Transaction.date_added == datetime(submit_time)))
             return transaction_id.scalar_one_or_none()
 
