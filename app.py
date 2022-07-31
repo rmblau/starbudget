a2de531a5e349b9b5cee16700bdd639b41daa925
Matthew <mrb1105@gmail.com>
2022-07-31 17:58:53 +0000

    revert changes to make ssl work again

--- app.py
+++ app.py
@@ -53,4 +53,4 @@ app.add_middleware(SessionMiddleware, secret_key="!secret", max_age=None)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000,
-                ssl_keyfile="/etc/letsencrypt/live/starbudget.rmblau.com/privkey.pem", ssl_certfile="/etc/letsencrypt/live/starbudget.rmblau.com/fullchain.pem", proxy_headers=True)
+                ssl_keyfile="/etc/starbudget/live/starbudget.rmblau.com/privkey.pem", ssl_certfile="/etc/starbudget/live/starbudget.rmblau.com/fullchain.pem", proxy_headers=True)
