f4958b94225ea66755daeaeaef5c376d0929f20b
Matthew Blau <matthew@saphira.mrb1105.gmail.com.beta.tailscale.net>
2022-07-31 21:41:47 -0400

    trying another workaround

--- views/views.py
+++ views/views.py
@@ -118,9 +118,9 @@ async def update_or_delete_transaction(request):
                 print(f'new description is {data["newname"]}')
                 transaction_id = await transaction.get_transaction_id(user_id=str(session_user['sub']), recipient=str(data['old_recipient']), amount=float(data['oldamount']), note=data['oldname'], date=date, category=str(data['oldcategory']), submit_time=datetime.strptime(str(submit_time), "%Y-%m-%d %H:%M:%S.%f"))
                 print(f' transaction_id is {transaction_id}')
-                await transaction.edit_transaction(recipient=data['newrecipient'],
-                                                   amount=float(data['newamount']), note=data['newname'], date_of_transactions=date,
-                                                   user_id=session_user['sub'], old_category_id=transaction_id, categories=data['category'], submit_time=submit_time)
+                await transaction.edit_transaction(
+                    amount=float(data['newamount']), recipient=data['newrecipient'], note=data['newname'], date_of_transactions=date,
+                    user_id=session_user['sub'], old_category_id=transaction_id, categories=data['category'], submit_time=datetime(submit_time))
             elif 'btnDeleteTransaction' in data:
                 print(data['submitTime'])
                 transaction_id = await transaction.get_transaction_id(user_id=session_user['sub'], recipient=data['old_recipient'], amount=data['oldamount'], note=data['oldname'], date=old_date, category=data['oldcategory'], submit_time=data['submitTime'])
